6e2b75db7af81ffb331892d522c782ae8619e39a
Abdirizak <abdirizaksigat99@gmail.com>
2022-10-20 12:02:02 +0300

    Added my project

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import React from "react"
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+let globalId= 0;
+function App() {
+  const [task , setTask]= useState("")
+  const [todos , setTodos]= useState([])
+
+  function createTodo(){
+     event.preventDefault()
+   setTodos(prevState=>{
+    setTask('')
+    return[...prevState , {todo:task, id: globalId++}]
+   })
+  }
+  function deleteHandle(itemID){
+    setTodos(prevState=> prevState.filter(item=>item.id !== itemID))
+
+  }
+  return <div>
+        <h1>BEST TODO APP</h1>
+        <form>
+        <input type="text" value={task} onChange={event=> {
+          setTask(event.target.value)
+        }}/>
+        <button onClick={createTodo} type="submit">Create todo</button>
+        </form>
+        <ul>
+          {todos.map(item=>{
+            return <div key={item.id }>
+            <li>{item.todo}</li> 
+            <button onClick={()=>deleteHandle(item.id)}>delete</button>
+            </div>})}
+          
+        </ul>
+      </div>
+    }
+
+export default App
